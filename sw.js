d9f44eed6177cf1334b8a51038d7689cd0f5a07e
c2kgyvg6f8-lab <c2kgyvg6f8@privaterelay.appleid.com>
2026-09-23 07:41:14 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='ccam-orl-v84-1';
+const F=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
